9848567c9937989c05f3fdb3db7827fba5827e53
trevrasher <93846352+trevrasher@users.noreply.github.com>
2025-09-29 11:37:19 -0700

    feat: start homepage

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <header className="title">
+      <h1 ><span className="spotify-green">Spotify</span>Tools</h1>
+        <h2>Playlist Management</h2>
+        <h2>YouTube to Spotify</h2>
+    </header>
+  );
+}
