c8104de1258a3b61ff31ac4d2b381f6edd7c6299
trevrasher <93846352+trevrasher@users.noreply.github.com>
2025-10-05 15:05:48 -0700

    feat: add UI for playlists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,26 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from 'react';
 import { useState } from "react";
-import { useSession, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { fetchLikedSongs, artistFilter, SpotifyTrackItem} from "./spotifyApi";
+
+interface ArtistPlaylist {
+  playlistName:string;
+  artists:string[];
+
+}
 
 export default function Home() {
   const { data: session, status } = useSession();
   const router = useRouter();
   const [likedSongs, setLikedSongs] = useState<SpotifyTrackItem[]| null | undefined> (null);
+  const [newPlaylist, setNewPlaylist] =useState('');
+  const [playlistArray, setPlaylistArray] = useState<ArtistPlaylist[]>([]);
+  const [selectedArtist, setSelectedArtist] = useState<string | null>(null);
+  const [placedArtists, setPlacedArtists] = useState<string[]>([]);
 
     useEffect(() => {
-    if (status === "loading") return;
+    if (status === "loading") return; 
     if (status === "unauthenticated") {
       router.push("../login");
     }
@@ -25,7 +35,15 @@ export default function Home() {
         }
     }, [status]);
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (newPlaylist.trim()) {
+      setPlaylistArray(prev => [...prev, { playlistName: newPlaylist, artists: [] }]);
+      setNewPlaylist('');
+    }
+  }
 
+    
   return (
     <>
       <header className="title">
@@ -33,15 +51,63 @@ export default function Home() {
           <h2>Playlist Management</h2>
           <h2>YouTube to Spotify</h2>
       </header>
+      <div className="main-content">
         <div>
           {likedSongs && Object.entries(artistFilter(likedSongs))
           .sort((a, b) => b[1].count - a[1].count)
           .map(([artist, info]) => (
-              <div key={artist} className="artist-listing">
+              <div className={`artist-listing${selectedArtist === artist ? " selected-artist" : ""}${placedArtists.includes(artist) ? " crossed-out" : ""}`} key={artist} onClick={() => setSelectedArtist(artist)}>
                 {artist}: {info.count}
                 </div>
             ))
           }
+        </div>
+          <div className="playlist-container">
+            <form onSubmit={handleSubmit} className="template-form">
+              <input 
+              type="text"
+              value={newPlaylist}
+              onChange={(e) => setNewPlaylist(e.target.value)}
+              placeholder="Add new playlist..."
+              className="playlist-input"
+              >
+              </input>
+            </form>
+            <div>
+             {playlistArray.map(({ playlistName, artists }, idx) => (
+            <div className="playlist"
+              key={playlistName + idx}
+              onClick={() => {
+                if (selectedArtist) {
+                  setPlaylistArray(prev =>
+                    prev.map((playlist, i) =>
+                      i === idx && !playlist.artists.includes(selectedArtist)
+                        ? { ...playlist, artists: [...playlist.artists, selectedArtist] }
+                        : playlist
+                    )
+                  );
+                  setSelectedArtist(null);
+                  setPlacedArtists(prev =>
+                  prev.includes(selectedArtist) ? prev : [...prev, selectedArtist]
+                );
+                }
+              }}
+            >
+                  <span className="playlist-text">{playlistName}</span>
+                  <button className="delete-button"
+                    onClick={() => {
+                      setPlaylistArray(prev =>
+                        prev.filter((_, i) => i !== idx)
+                      );
+                    }}>X
+                  </button>
+                  {artists.map((artist, idx) => (
+                    <div key={artist+idx} className="artist-in-playlist">{artist}</div>
+                  ))}
+                </div>
+              ))}           
+            </div>
+          </div>
         </div>
     </>
   );
